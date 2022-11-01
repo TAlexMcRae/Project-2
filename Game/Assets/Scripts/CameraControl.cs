22541f415526d780cae28880c74510c7c9ac91b8
Timothy "Alex" McRae <TAMcRae@student.fullsail.edu>
2022-11-01 14:48:31 -0400

    Finished Camera
    Added camera control functionality. - Alex

--- Game/Assets/Scripts/CameraControl.cs
+++ Game/Assets/Scripts/CameraControl.cs
@@ -5,13 +5,36 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
 
+    #region Variables
+    [SerializeField] int senseHor;
+    [SerializeField] int senseVer;
+    [SerializeField] bool invertY;
+
+    [SerializeField] int vertMin;
+    [SerializeField] int vertMax;
+    float xRotation;
+    #endregion
+
     void Start()
     {
-        
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
-        
+
+        // gets mouse input
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * senseHor;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * senseVer;
+
+        if (invertY) { xRotation += mouseY; }
+        else { xRotation -= mouseY; }
+
+        // rotates camera with limits when turn on X-Axis
+        xRotation = Mathf.Clamp(xRotation, vertMin, vertMax);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.parent.Rotate(Vector3.up * mouseX);
     }
 }
