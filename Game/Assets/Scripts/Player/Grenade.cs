71b33bdaa2ca874cbafde7da4440e628ef3db7a2
Samy <samy8xx@gmail.com>
2022-12-09 13:40:40 -0500

    Grenade skin and visual effects
    Now the grenade looks like an explosive and has explosion particle effects

--- Game/Assets/Scripts/Player/Grenade.cs
+++ Game/Assets/Scripts/Player/Grenade.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-
     public bool exploding;
+    [SerializeField] GameObject explosionDamage;
     [SerializeField] GameObject explosionEffect;
 
     void OnTriggerEnter(Collider other)
     {
-        Instantiate(explosionEffect, gameObject.transform.position, gameObject.transform.rotation);
-        Destroy(gameObject);
+        GameObject boomParticles = Instantiate(explosionEffect, gameObject.transform.position, gameObject.transform.rotation);
+        Destroy(boomParticles, 0.4f);
     }
 }
