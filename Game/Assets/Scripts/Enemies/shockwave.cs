e039380d23728b6fd9d7cfcb3186c1c09851e903
Kassius McKenzie <KMcKenzie@student.fullsail.edu>
2022-12-09 14:38:24 -0500

    UI Changes and Some SFX

--- Game/Assets/Scripts/Enemies/shockwave.cs
+++ Game/Assets/Scripts/Enemies/shockwave.cs
@@ -7,6 +7,7 @@ public class shockwave : MonoBehaviour
     [SerializeField] bossBearAI bossBearAI;
     [SerializeField] private float knockbackStrength;
     // Start is called before the first frame update
+
     private void OnTriggerEnter(Collider collision)
     {
         bossBearAI.swDamage();
